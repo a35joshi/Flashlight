aac0c4e39e07e0e99005a769a1c2f82cc28464fc
Anurag Joshi <a35joshi@uwaterloo.ca>
2016-08-15 17:24:51 +0400

    InitialSetup

---
+++ app/src/main/java/flashlightapp/flashlight/Flashlight.java
@@ -0,0 +1,13 @@
+package flashlightapp.flashlight;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Flashlight extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_flashlight);
+    }
+}
