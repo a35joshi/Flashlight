29b6c6a72d6123dfee8754b87b8e15732762ed5a
Anurag Joshi <a35joshi@uwaterloo.ca>
2016-08-16 12:23:36 +0400

    InitialSetup

--- app/src/main/java/flashlightapp/flashlight/Flashlight.java
+++ app/src/main/java/flashlightapp/flashlight/Flashlight.java
@@ -1,11 +1,38 @@
 package flashlightapp.flashlight;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Flashlight extends AppCompatActivity {
+
+    Button onoff;
+    boolean clicked=false;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashlight);
+        InitialiseViews();
+        onoff.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //click detected!
+                clicked = true;
+                FlashlightLogic();
+            }
+        });
+    }
+    void InitialiseViews() {
+        try {
+       onoff=(Button)findViewById(R.id.ONOFFBUTTON);
+        }
+catch (Exception ex){
+    throw ex;
+}
+
+    }
+    void FlashlightLogic(){
+        
     }
 }
+
