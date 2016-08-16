f344127a8cfdbce880b97a0be04d07810a5424ff
Anurag Joshi <a35joshi@uwaterloo.ca>
2016-08-16 17:13:09 +0400

    Fully working Torch

--- app/src/main/java/flashlightapp/flashlight/Flashlight.java
+++ app/src/main/java/flashlightapp/flashlight/Flashlight.java
@@ -1,38 +1,152 @@
 package flashlightapp.flashlight;
+//Basic application to create a torch.
+//BY ANURAG JOSHI
+//UNIVERSITY OF WATERLOO.
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.pm.PackageManager;
+import android.hardware.Camera;
+import android.hardware.Camera.Parameters;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
 public class Flashlight extends AppCompatActivity {
-
     Button onoff;
-    boolean clicked=false;
+    private Camera myCamera;
+    private boolean FlashOn;
+    private boolean FlashSupport;
+    Parameters myParameters;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashlight);
-        InitialiseViews();
+        onoff=(Button)findViewById(R.id.onoffbutton);
+        //checking if hardware supports torchlight or not!
+        FlashSupport = getApplicationContext().getPackageManager()
+                .hasSystemFeature(PackageManager.FEATURE_CAMERA_FLASH);
+        //if not then throw error
+        if (!FlashSupport) {
+            // device doesn't support flash
+            // Show alert message and close the application
+            AlertDialog alert = new AlertDialog.Builder(Flashlight.this)
+                    .create();
+            alert.setTitle("Error");
+            alert.setMessage("YOUR DEVICE HARDWARE DOESN'T SUPPORT FLASHLIGHT");
+            alert.setButton("OK", new DialogInterface.OnClickListener() {
+                public void onClick(DialogInterface dialog, int which) {
+                    // closing the application
+                    finish();
+                }
+            });
+            alert.show();
+            return;
+        }
+
         onoff.setOnClickListener(new View.OnClickListener() {
+
             @Override
             public void onClick(View v) {
-                //click detected!
-                clicked = true;
-                FlashlightLogic();
+                //Get Camera features
+                getCamera();
+                if (FlashOn) {
+                    // turn off flash
+                    TurnOffFlash();
+                } else {
+                    // turn on flash
+                    TurnOnFlash();
+                }
             }
         });
     }
-    void InitialiseViews() {
-        try {
-       onoff=(Button)findViewById(R.id.ONOFFBUTTON);
+
+
+    // Get the camera
+    private void getCamera() {
+        if (myCamera == null) {
+            try {
+                myCamera = Camera.open();
+                myParameters = myCamera.getParameters();
+            } catch (RuntimeException e) {
+                //need to give app permission explicitly for Android 6.0 and upwards.
+                throw e;
+            }
         }
-catch (Exception ex){
-    throw ex;
-}
+    }
+
+    private void TurnOnFlash() {
+        if (!FlashOn) {
+            if (myCamera == null || myParameters == null) {
+                return;
+            }
+            myParameters = myCamera.getParameters();
+            myParameters.setFlashMode(Parameters.FLASH_MODE_TORCH);
+            myCamera.setParameters(myParameters);
+            myCamera.startPreview();
+            FlashOn = true;
+        }
 
     }
-    void FlashlightLogic(){
-        
+    private void TurnOffFlash() {
+        if (FlashOn) {
+            if (myCamera == null || myParameters == null) {
+                return;
+            }
+            myParameters = myCamera.getParameters();
+            myParameters.setFlashMode(Parameters.FLASH_MODE_OFF);
+            myCamera.setParameters(myParameters);
+            myCamera.stopPreview();
+            FlashOn = false;
+     }
     }
-}
 
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+
+        // on pause turn off the flash
+        TurnOffFlash();
+    }
+
+    @Override
+    protected void onRestart() {
+        FlashOn=false;
+        super.onRestart();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        // on resume turn on the flash
+       // if(FlashSupport)
+         //   TurnOnFlash();
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+
+        // on starting the app get the camera params
+        getCamera();
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+
+        // on stop release the camera
+        if (myCamera != null) {
+            myCamera.release();
+            myCamera = null;
+        }
+    }
+
+}
